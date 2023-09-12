9afed9ed3c70465c516f6bca832b1d46f92dc8a9
NattyNgrn <natnrgn@gmail.com>
2023-09-12 15:45:14 -0400

    setting up app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import Addevent from './components/addevent'
+import Bar from './components/bar'
+import Eventlist from './components/eventlist'
+import Searchbar from './components/searchbar'
 
 function App() {
 
   return (
       <div>
-
+        <h1>Eventonica</h1>
+        <Addevent></Addevent>
+        <Searchbar></Searchbar>
+        <Bar></Bar>
+        <Eventlist></Eventlist>
       </div>
   )
 }
