2e7384c3f1ee961e06a063f21c488481c4547ed4
NattyNgrn <natnrgn@gmail.com>
2023-09-12 15:40:45 -0400

    working on search bar component

--- client/src/components/searchbar.jsx
+++ client/src/components/searchbar.jsx
@@ -2,7 +2,11 @@ function Searchbar() {
 
     return (
         <div>
-
+            <form>
+                <label>Search</label>
+                <input></input>
+                <button>Search</button>
+            </form>
         </div>
     )
 }
