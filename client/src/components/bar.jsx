37595f7cc7fb39b88334070afd98c685cc4c4dd7
NattyNgrn <natnrgn@gmail.com>
2023-09-12 15:45:57 -0400

    working on bar component

--- client/src/components/bar.jsx
+++ client/src/components/bar.jsx
@@ -2,7 +2,11 @@ function Bar() {
 
     return (
         <div>
-
+            <h2>Favorite</h2>
+            <h2>Name</h2>
+            <h2>Date</h2>
+            <h2>Description</h2>
+            <h2>Edit</h2>
         </div>
     )
 }
