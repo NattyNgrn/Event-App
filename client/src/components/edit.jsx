e52701e98bf964f940dbdb05677911c2151b40c6
NattyNgrn <natnrgn@gmail.com>
2023-09-12 15:36:57 -0400

    working on edit component

--- client/src/components/edit.jsx
+++ client/src/components/edit.jsx
@@ -2,7 +2,16 @@ function Edit() {
 
     return (
         <div>
-
+            <form>
+                <label>Name</label>
+                <input></input>
+                <label>Date</label>
+                <input></input>
+                <label>Description</label>
+                <input></input>
+                <button>Save</button>
+                <button>Delete</button>
+            </form>
         </div>
     )
 }
