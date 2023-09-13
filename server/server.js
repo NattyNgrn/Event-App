a330ca7f7646bb965499c68ca162c360739dc475
NattyNgrn <natnrgn@gmail.com>
2023-09-13 15:47:42 -0400

    YES

--- server/server.js
+++ server/server.js
@@ -1,3 +1,29 @@
-import { Express } from "express";
+import { express } from "express";
 import { cors } from "cors"
+const app = express();
+app.use(cors());
 
+const port = process.env.PORT || 6666;
+const DB = "";
+
+async function getDate(DB){
+
+}
+
+app.get("/", (req, res) => {
+    res.send("WELCOME");
+});
+
+app.post(){
+
+}
+
+app.put(){
+
+}
+
+app.delete(){
+    
+}
+
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
